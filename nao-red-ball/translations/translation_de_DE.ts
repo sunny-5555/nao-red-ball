b8d19f0b6bbab3de4febdb2758459c779502cee3
sunny-5555 <83015830+sunny-5555@users.noreply.github.com>
2022-05-28 21:42:58 +0200

    Revert "delete unnecessary files"
    This reverts commit 8f200e7384da1bd679365b2a3d88421f11093f07.

---
+++ nao-red-ball/translations/translation_de_DE.ts
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+    <context>
+        <name>behavior_1/behavior.xar:/Ball found</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>ball found</source>
+            <comment>Text</comment>
+            <translation type="unfinished">ball found</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Ball not found</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>No ball</source>
+            <comment>Text</comment>
+            <translation type="unfinished">No ball</translation>
+        </message>
+    </context>
+</TS>
